b663e8a15af79c1fed5f7ca609ccd5deecf5ebed
cellocamara <marcellocamara@id.uff.br>
2018-08-25 21:57:35 -0300

    Adicionado Fragmnents

--- app/src/main/java/com/dev/hacknit/Fragments/Boletim4Fragment.java
+++ app/src/main/java/com/dev/hacknit/Fragments/Boletim4Fragment.java
@@ -1,27 +1,32 @@
 package com.dev.hacknit.Fragments;
 
+
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.dev.hacknit.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Boletim2Fragment extends Fragment {
+public class Boletim4Fragment extends Fragment {
 
 
-    public Boletim2Fragment() {
+    public Boletim4Fragment() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_boletim2, container, false);
+        return inflater.inflate(R.layout.fragment_boletim4, container, false);
     }
 
 }
