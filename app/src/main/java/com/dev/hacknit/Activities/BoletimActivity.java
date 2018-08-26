81d57f937ab2788da350087bb38bf29c699d93e3
cellocamara <marcellocamara@id.uff.br>
2018-08-25 21:01:36 -0300

    Adicionado Fragmnents

--- app/src/main/java/com/dev/hacknit/Activities/BoletimActivity.java
+++ app/src/main/java/com/dev/hacknit/Activities/BoletimActivity.java
@@ -1,10 +1,18 @@
 package com.dev.hacknit.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
+
 import com.dev.hacknit.R;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class BoletimActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private SmartTabLayout smartTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +22,22 @@ public class BoletimActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-    }
+        getSupportActionBar().setElevation(0);
 
+        viewPager = findViewById(R.id.viewPager);
+        smartTabLayout = findViewById(R.id.smartTabLayout);
+
+        //Configurando o adapter e criando as abas que serão fragments
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("1", Boletim1Fragment.class)
+                .add("2", Boletim2Fragment.class)
+                .create()
+        );
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+        viewPager.setCurrentItem(0);
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
---
+++ app/src/main/java/com/dev/hacknit/Fragments/Boletim1Fragment.java
@@ -0,0 +1,31 @@
+package com.dev.hacknit.Fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dev.hacknit.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Boletim1Fragment extends Fragment {
+
+
+    public Boletim1Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_boletim1, container, false);
+    }
+
+}
